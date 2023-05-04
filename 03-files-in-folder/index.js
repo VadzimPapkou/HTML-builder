1c6e92ecf96a66464a99519fff92a65c2a5c626c
Vadim <playvarian1@gmail.com>
2023-05-04 03:16:05 +0300

    feat: complete 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+async function readfiles(from) {
+  const nodes = await fs.readdir(from);
+  const nodesStats = await Promise.all(nodes.map(async function (node) {
+    return await fs.lstat(path.join(from, node));
+  }));
+  const nodesWithStats = nodes.map((node, i) => ({node, stats: nodesStats[i]}));
+
+  const result = nodesWithStats.filter(({stats}) => stats.isFile())
+    .map(({node, stats}) => {
+      const size = stats.size;
+      const {name, ext} = path.parse(node);
+      return [name, ext, size].join(' - ');
+    })
+    .join('\n');
+  console.log(result);
+}
+
+readfiles(path.join(__dirname, 'secret-folder'));
