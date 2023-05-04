dda7665fbce60f6206a9e353b77fe479fda5e567
Vadim <playvarian1@gmail.com>
2023-05-04 16:45:29 +0300

    feat: 02-write-file type exit to console stop program

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,12 +5,16 @@ const path = require('path');
 const OUTPUT = path.join(__dirname, 'text.txt');
 
 async function main() {
-  await fs.writeFile(OUTPUT, '');
+  await fs.appendFile(OUTPUT, '');
   console.log('Ready for writing...');
 
-  process.stdin.pipe(
-    createWriteStream(OUTPUT)
-  );
+  const ws = createWriteStream(OUTPUT);
+
+  process.stdin.on('data', data => {
+    const stringInput = data.toString();
+    if(stringInput.startsWith('exit')) return process.emit('SIGINT');
+    ws.write(stringInput);
+  });
 
   process.on('SIGINT', () => {
     console.log('Bye');
